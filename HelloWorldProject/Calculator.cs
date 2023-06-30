055da265d72d421f1a890371c270788211b2d7c0
rahulkgautam <rahulkgautam125@gmail.com>
2023-07-01 00:37:51 +0530

    Create Calculator program

---
+++ HelloWorldProject/Calculator.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HelloWorldProject
+{
+    class Calculator
+    {
+        public double Addition(double firstNumber,double secondNumber)
+        {
+            return firstNumber + secondNumber;
+        }        
+        public double Substration(double firstNumber, double secondNumber)
+        {
+            return firstNumber - secondNumber;
+        }       
+        public double Multipilcation(double firstNumber, double secondNumber)
+        {
+            return firstNumber * secondNumber;
+        }        
+        public double Division(double firstNumber, double secondNumber)
+        {
+            return firstNumber / secondNumber;
+        }        
+    }
+}
---
+++ HelloWorldProject/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace HelloWorldProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to calculator program");
+            Console.WriteLine("Enter first Number");
+            double firstNumber = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter second Number");
+            double secondNumber = Convert.ToDouble(Console.ReadLine());
+            Calculator calculator = new Calculator();
+
+            double result = calculator.Addition(firstNumber, secondNumber);
+            Console.WriteLine("Add: " + result);
+            //double resultSub = calculator.Substration(firstNumber, secondNumber);
+            //Console.WriteLine("Sub: " + resultSub);
+            //double resultMul = calculator.Multipilcation(firstNumber, secondNumber);
+            //Console.WriteLine("Mul: " + resultMul);
+            //double resultDiv = calculator.Division(firstNumber, secondNumber);
+            //Console.WriteLine("Div: " + resultDiv);
+
+        }
+
+    }
+}
